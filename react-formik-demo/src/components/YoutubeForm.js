95d6aaeb55d57068858a5554fbd0b419e86ab512
Denis Yuzhelevsky <denislevsky@gmail.com>
2022-01-15 21:15:14 +0200

    dev

--- react-formik-demo/src/components/YoutubeForm.js
+++ react-formik-demo/src/components/YoutubeForm.js
@@ -1,163 +1,167 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import {
   Formik,
   Form,
   Field,
   ErrorMessage,
   FieldArray,
-  FastField
-} from 'formik'
-import * as Yup from 'yup'
-import TextError from './TextError'
+  FastField,
+} from "formik";
+import * as Yup from "yup";
+import TextError from "./TextError";
 
 const initialValues = {
-  name: 'Vishwas',
-  email: '',
-  channel: '',
-  comments: '',
-  address: '',
+  name: "Vishwas",
+  email: "",
+  channel: "",
+  comments: "",
+  address: "",
   social: {
-    facebook: '',
-    twitter: ''
+    facebook: "",
+    twitter: "",
   },
-  phoneNumbers: ['', ''],
-  phNumbers: ['']
-}
+  phoneNumbers: ["", ""],
+  phNumbers: [""],
+};
 
 const savedValues = {
-  name: 'Vishwas',
-  email: 'v@example.com',
-  channel: 'codevolution',
-  comments: 'Welcome to Formik',
-  address: '221B Baker Street',
+  name: "Vishwas",
+  email: "v@example.com",
+  channel: "codevolution",
+  comments: "Welcome to Formik",
+  address: "221B Baker Street",
   social: {
-    facebook: '',
-    twitter: ''
+    facebook: "",
+    twitter: "",
   },
-  phoneNumbers: ['', ''],
-  phNumbers: ['']
-}
+  phoneNumbers: ["", ""],
+  phNumbers: [""],
+};
 
 const onSubmit = (values, submitProps) => {
-  console.log('Form data', values)
-  console.log('submitProps', submitProps)
-  submitProps.setSubmitting(false)
-  submitProps.resetForm()
-}
+  console.log("Form data", values);
+  console.log("submitProps", submitProps);
+  submitProps.setSubmitting(false);
+  submitProps.resetForm();
+};
 
 const validationSchema = Yup.object({
-  name: Yup.string().required('Required'),
-  email: Yup.string()
-    .email('Invalid email format')
-    .required('Required'),
-  channel: Yup.string().required('Required'),
-  comments: Yup.string().required('Required')
-})
+  name: Yup.string().required("Required"),
+  email: Yup.string().email("Invalid email format").required("Required"),
+  channel: Yup.string().required("Required"),
+  comments: Yup.string().required("Required"),
+});
 
-const validateComments = value => {
-  let error
+const validateComments = (value) => {
+  let error;
   if (!value) {
-    error = 'Required'
+    error = "Required";
   }
-  return error
-}
+  return error;
+};
 
-function YoutubeForm () {
-  const [formValues, setFormValues] = useState(null)
+function YoutubeForm() {
+  const [formValues, setFormValues] = useState(null);
+  console.log("rendering component");
   return (
     <Formik
       initialValues={formValues || initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
       enableReinitialize
-      // validateOnChange={false}
+      validateOnChange={false}
       // validateOnBlur={false}
       // validateOnMount
     >
-      {formik => {
-        console.log('Formik props', formik)
+      {(formik) => {
+        console.log("Formik props", formik);
         return (
           <Form>
-            <div className='form-control'>
-              <label htmlFor='name'>Name</label>
-              <Field type='text' id='name' name='name' />
-              <ErrorMessage name='name' component={TextError} />
+            <div className="form-control">
+              <label htmlFor="name">Name</label>
+              <Field type="text" id="name" name="name" />
+              <ErrorMessage name="name" component={TextError} />
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='email'>Email</label>
-              <Field type='email' id='email' name='email' />
-              <ErrorMessage name='email'>
-                {error => <div className='error'>{error}</div>}
+            <div className="form-control">
+              <label htmlFor="email">Email</label>
+              <Field type="email" id="email" name="email" />
+              <ErrorMessage name="email">
+                {(error) => <div className="error">{error}</div>}
               </ErrorMessage>
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='channel'>Channel</label>
+            <div className="form-control">
+              <label htmlFor="channel">Channel</label>
               <Field
-                type='text'
-                id='channel'
-                name='channel'
-                placeholder='YouTube channel name'
+                type="text"
+                id="channel"
+                name="channel"
+                placeholder="YouTube channel name"
               />
-              <ErrorMessage name='channel' />
+              <ErrorMessage name="channel" />
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='comments'>Comments</label>
+            <div className="form-control">
+              <label htmlFor="comments">Comments</label>
               <Field
-                as='textarea'
-                id='comments'
-                name='comments'
+                as="textarea"
+                id="comments"
+                name="comments"
                 validate={validateComments}
-              />
-              <ErrorMessage name='comments' component={TextError} />
+              >
+                {({ field, form, meta }) => {
+                  console.log("Field render", form.errors);
+                  return <Field as="textarea" id="comments" name="comments" />;
+                }}
+              </Field>
+              <ErrorMessage name="comments" component={TextError} />
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='address'>Address</label>
-              <FastField name='address'>
+            <div className="form-control">
+              <label htmlFor="address">Address</label>
+              <FastField name="address">
                 {({ field, form, meta }) => {
-                  // console.log('Field render')
+                  console.log("Fast Field render", form.errors);
                   return (
                     <div>
-                      <input type='text' {...field} />
+                      <input type="text" {...field} />
                       {meta.touched && meta.error ? (
                         <div>{meta.error}</div>
                       ) : null}
                     </div>
-                  )
+                  );
                 }}
               </FastField>
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='facebook'>Facebook profile</label>
-              <Field type='text' id='facebook' name='social.facebook' />
+            <div className="form-control">
+              <label htmlFor="facebook">Facebook profile</label>
+              <Field type="text" id="facebook" name="social.facebook" />
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='twitter'>Twitter profile</label>
-              <Field type='text' id='twitter' name='social.twitter' />
+            <div className="form-control">
+              <label htmlFor="twitter">Twitter profile</label>
+              <Field type="text" id="twitter" name="social.twitter" />
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='primaryPh'>Primary phone number</label>
-              <Field type='text' id='primaryPh' name='phoneNumbers[0]' />
+            <div className="form-control">
+              <label htmlFor="primaryPh">Primary phone number</label>
+              <Field type="text" id="primaryPh" name="phoneNumbers[0]" />
             </div>
 
-            <div className='form-control'>
-              <label htmlFor='secondaryPh'>Secondary phone number</label>
-              <Field type='text' id='secondaryPh' name='phoneNumbers[1]' />
+            <div className="form-control">
+              <label htmlFor="secondaryPh">Secondary phone number</label>
+              <Field type="text" id="secondaryPh" name="phoneNumbers[1]" />
             </div>
 
-            <div className='form-control'>
+            <div className="form-control">
               <label>List of phone numbers</label>
-              <FieldArray name='phNumbers'>
-                {fieldArrayProps => {
-                  const { push, remove, form } = fieldArrayProps
-                  const { values } = form
-                  const { phNumbers } = values
+              <FieldArray name="phNumbers">
+                {(fieldArrayProps) => {
+                  const { push, remove, form } = fieldArrayProps;
+                  const { values } = form;
+                  const { phNumbers } = values;
                   // console.log('fieldArrayProps', fieldArrayProps)
                   // console.log('Form errors', form.errors)
                   return (
@@ -166,17 +170,17 @@ function YoutubeForm () {
                         <div key={index}>
                           <Field name={`phNumbers[${index}]`} />
                           {index > 0 && (
-                            <button type='button' onClick={() => remove(index)}>
+                            <button type="button" onClick={() => remove(index)}>
                               -
                             </button>
                           )}
                         </div>
                       ))}
-                      <button type='button' onClick={() => push('')}>
+                      <button type="button" onClick={() => push("")}>
                         +
                       </button>
                     </div>
-                  )
+                  );
                 }}
               </FieldArray>
             </div>
@@ -208,21 +212,21 @@ function YoutubeForm () {
             >
               Visit all
             </button> */}
-            <button type='button' onClick={() => setFormValues(savedValues)}>
+            <button type="button" onClick={() => setFormValues(savedValues)}>
               Load saved data
             </button>
-            <button type='reset'>Reset</button>
+            <button type="reset">Reset</button>
             <button
-              type='submit'
+              type="submit"
               disabled={!formik.isValid || formik.isSubmitting}
             >
               Submit
             </button>
           </Form>
-        )
+        );
       }}
     </Formik>
-  )
+  );
 }
 
-export default YoutubeForm
+export default YoutubeForm;
